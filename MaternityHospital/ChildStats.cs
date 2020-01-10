7605ad672c90488c71c98ba5a86631f80fbe3fcd
Filip Margitai <u18.415@pslib.cloud>
2020-01-10 08:26:08 +0100

    mám to fakt hustě rozjetý

---
+++ MaternityHospital/ChildStats.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaternityHospital
+{
+    class ChildStats : BaseMHStats
+    {
+        private Dictionary<string, double> ChildDictionary = new Dictionary<string, double>();
+        public override void AddChild(string name, double weight)
+        {
+            ChildDictionary.Add(name, weight);
+        }
+
+        public override string GetCommonNames()
+        {
+            List<string> CommonNames = new List<string> { };
+            int NameCount = 0;
+            for(int i = 0; i < ChildDictionary.Count; i++)
+            {
+
+            }
+        }
+
+        public override string GetFatNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetFatWeight()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
